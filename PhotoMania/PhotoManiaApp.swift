1b6319bb0a0e3a0cd9988fde079fad9c4df910d8
Julie Go <julie.gosselin84@yahoo.fr>
2022-07-12 17:09:00 +0200

    Initial Commit

---
+++ PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by User on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
